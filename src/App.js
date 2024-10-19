b582c1e0d830749c65bdba21bef9dc4052489496
Your Name <you@example.com>
2024-10-19 08:24:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './commponants/header/header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
